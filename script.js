608a6a6d6312804980ec06625f05143474bbf59b
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-20 19:39:23 +0200

    Add discounted price method

--- script.js
+++ script.js
@@ -22,6 +22,25 @@ class Product {
 	}
 }
 
+class DiscountProduct extends Product {
+	constructor(productName, price, stockQuantity, discount) {
+		super(productName, price, stockQuantity);
+		this.discount = discount;
+	}
+
+	getDiscountedPrice() {
+		let price = this.price - this.price * (this.discount / 100);
+		return price;
+	}
+
+	logDiscountedPrice() {
+		let DiscountedPrice = this.getDiscountedPrice();
+		console.log(
+			`Price with discount for ${this.productName} is ${DiscountedPrice}`
+		);
+	}
+}
+
 const bike = new Product(`Bicicleta`, 750, 1000);
 
 // este echivalentul:
@@ -35,15 +54,23 @@ const product2 = {
 const laptop = new Product(`Laptop`, 2500, 300);
 
 console.log(bike);
-
 console.log(product2);
-
 console.log(laptop);
 
 bike.addToStock(100);
-
 console.log(bike);
 
 laptop.removeFromStock(25);
-
 console.log(laptop);
+
+const smartphone = new DiscountProduct(`Smartphone`, 3000, 200, 20);
+
+console.log(smartphone);
+smartphone.removeFromStock(23);
+
+// const phoneDiscountedPrice = smartphone.getDiscountedPrice();
+// console.log(
+// 	`Price with discount for ${smartphone.productName} is ${phoneDiscountedPrice}`
+// );
+
+smartphone.logDiscountedPrice();
